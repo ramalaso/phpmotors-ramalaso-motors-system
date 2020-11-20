f06657ae654a1aa0afe08f9ed98e383fff280cc3
Raul Laredo Soria <Raul.Laredo@jalasoft.com>
2020-11-19 21:19:31 -0400

    Added detail page styling

--- view/vehicle-detail.php
+++ view/vehicle-detail.php
@@ -5,7 +5,6 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="/css/style.css" media="screen" />
-  <link rel="stylesheet" href="../css/styleLogin.css" media="screen" />
   <title>PHP Motors</title>
 </head>
 
@@ -18,12 +17,13 @@
       <?php echo $navList; ?>
     </nav>
     <main>
-    <h1> <?php if(isset($vehicleData['invMake'])) {echo "$vehicleData[invMake]";} ?> </h1>
-    <h2> <?php if(isset($vehicleData['invDescription'])) {echo "$vehicleData[invDescription]";} ?></h2>
-   <h2> <?php if(isset($vehicleData['invPrice'])) {echo "$vehicleData[invPrice]";} ?></h2>
-   <h2> <?php if(isset($vehicleData['invModel'])) {echo "$vehicleData[invModel]";} ?></h2>
-   <img src='<?php if(isset($vehicleData['invThumbnail'])) {echo "$vehicleData[invThumbnail]";} ?>' />
-   
+        <div class="container-detail">
+            <img src='<?php if(isset($vehicleData['invThumbnail'])) {echo "$vehicleData[invThumbnail]";} ?>' />
+            <h1> <?php if(isset($vehicleData['invMake'])) {echo "$vehicleData[invMake]";} ?> </h1>
+            <h4> <?php if(isset($vehicleData['invDescription'])) {echo "$vehicleData[invDescription]";} ?></h4>
+            <h4> <?php if(isset($vehicleData['invPrice'])) {echo "$vehicleData[invPrice]";} ?></h4>
+            <h4> <?php if(isset($vehicleData['invModel'])) {echo "$vehicleData[invModel]";} ?></h4>
+        </div>   
     </main>
     <footer>
       <?php require $_SERVER['DOCUMENT_ROOT'].'/snippets/footer.php'?>
