2bfdc14b39f870e24162a8a999f9a28cdf88d3ac
Raul Laredo Soria <Raul.Laredo@jalasoft.com>
2020-11-19 22:28:24 -0400

    Added details

--- view/vehicle-detail.php
+++ view/vehicle-detail.php
@@ -21,8 +21,11 @@
             <img src='<?php if(isset($vehicleData['invThumbnail'])) {echo "$vehicleData[invThumbnail]";} ?>' />
             <h1> <?php if(isset($vehicleData['invMake'])) {echo "$vehicleData[invMake]";} ?> </h1>
             <h4> <?php if(isset($vehicleData['invDescription'])) {echo "$vehicleData[invDescription]";} ?></h4>
-            <h4> <?php if(isset($vehicleData['invPrice'])) {echo "$".number_format("$vehicleData[invPrice]", 2);} ?></h4>
-            <h4> <?php if(isset($vehicleData['invModel'])) {echo "$vehicleData[invModel]";} ?></h4>
+            <h4> Price: <?php if(isset($vehicleData['invPrice'])) {echo "$".number_format("$vehicleData[invPrice]", 2);} ?></h4>
+            <h4> Model: <?php if(isset($vehicleData['invModel'])) {echo "$vehicleData[invModel]";} ?></h4>
+            <h4> Color: <?php if(isset($vehicleData['invColor'])) {echo "$vehicleData[invColor]";} ?></h4>
+            <h4> Stock: <?php if(isset($vehicleData['invStock'])) {echo "$vehicleData[invStock]";} ?></h4>
+
         </div>   
     </main>
     <footer>
